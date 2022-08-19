9fd01da1dafdd7f4857b4e2c1e2773ed44cb1d0c
mateusz.kowalke <mateusz.kowalke@aurafutures.com>
2022-08-19 14:47:27 +0200

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
+
